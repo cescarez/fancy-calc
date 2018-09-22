3a43b82fe1da3f5e25dbbc7dcb342002112f7f47
Christabel Escarez <christabel.escarez@gmail.com>
2018-09-22 09:38:04 -0700

    introduce if condition to number button/append input to #output

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,13 @@ var divide =  function(number1, number2) {
 };
 
 $(document).ready(function() {
-  $(".number").click(function(event) {
+  $("button").click(function(event) {
     event.preventDefault();
-    var input = parseInt($(this).val());
-    $("#output").append(input);
 
-    // if (operator==="add") {
-    //   var result = add(number1, number2);
-    //   $("#output").text(result);
-    // } else if (operator==="subtract") {
-    //   var result = subtract(number1, number2);
-    //   $("#output").text(result);
-    // } else if (operator==="multiply") {
-    //   var result = multiply(number1, number2);
-    //   $("#output").text(result);
-    // } else if (operator==="divide") {
-    //   var result = divide(number1, number2);
-    //   $("#output").text(result);
-    // }
+    if (".number") {
+      var input = parseInt($(this).val());
+      $("#output").append(input);
+    }
+
   });
 });
