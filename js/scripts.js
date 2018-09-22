0da475193270eee0e479d780ab6f911d27247c7e
Christabel Escarez <christabel.escarez@gmail.com>
2018-09-22 09:28:17 -0700

    change button opacity on hover, add values to buttons

--- js/scripts.js
+++ js/scripts.js
@@ -12,26 +12,23 @@ var divide =  function(number1, number2) {
 };
 
 $(document).ready(function() {
-  $("form#calc").submit(function(event) {
+  $(".number").click(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#num1").val());
-    var number2 = parseInt($("#num2").val());
-    var operator = $("#operator").val();
-    var num1type = typeof number1;
-    var num2type = typeof number2;
+    var input = parseInt($(this).val());
+    $("#output").append(input);
 
-    if (operator==="add") {
-      var result = add(number1, number2);
-      $("#output").text(result);
-    } else if (operator==="subtract") {
-      var result = subtract(number1, number2);
-      $("#output").text(result);
-    } else if (operator==="multiply") {
-      var result = multiply(number1, number2);
-      $("#output").text(result);
-    } else if (operator==="divide") {
-      var result = divide(number1, number2);
-      $("#output").text(result);
-    }
+    // if (operator==="add") {
+    //   var result = add(number1, number2);
+    //   $("#output").text(result);
+    // } else if (operator==="subtract") {
+    //   var result = subtract(number1, number2);
+    //   $("#output").text(result);
+    // } else if (operator==="multiply") {
+    //   var result = multiply(number1, number2);
+    //   $("#output").text(result);
+    // } else if (operator==="divide") {
+    //   var result = divide(number1, number2);
+    //   $("#output").text(result);
+    // }
   });
 });
